06a77012486d0021b0762574177be15e595ee22c
BDedafoe <bmd6260@gmail.com>
2023-01-31 07:34:11 -0600

    added calendar view

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-        //useFindAndModify: false     //no longer need this as Mongoose 6 always sets useFindAndModify as false and is no longer supported
     })
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
--- index.js
+++ index.js
@@ -62,5 +62,6 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'))
 app.use('/users', require('./routes/users.js'))
 app.use('/auth', require('./routes/auth.js'))
+app.use('/calendar', require('./routes/calendar.js'))
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
